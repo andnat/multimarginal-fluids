77870bdd29970f606f473b2178ef559ff86fb573
andnat <nichel27@gmail.com>
2017-12-08 21:16:35 +0100

    Camassa Holm solver

--- chconesolver.py
+++ chconesolver.py
@@ -10,47 +10,49 @@ import multimarg_fluids as mm
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 5 
-Nr = 20 
+Nx = 20 
+Nr = 5 
 
 L = 1.0
-h = L/N
+h = L/Nx
 hr = np.pi/2.0/(Nr+1)
 K = 16 
-eps = 0.01
+eps = 0.05
 
 # Parameters for cone metric
 a = 1.0
 b= 0.5
 
-X0 = np.linspace(0.0,L,N)
-X1 = np.linspace(hr,np.pi/2.0,Nr,endpoint = False)
+X0 = np.linspace(0.0,L,Nx)
+#X1 = np.asarray([1.0]) 
+X1= np.exp(np.linspace(-1.0,1.0,Nr))
 
 X0m,X1m = np.meshgrid(X0,X1)
 X =[X0m,X1m]
 
 # Marginal using x1 = arctan(r) change of variable
-nu = (np.tan(X1m.flatten()))**(-3)*(np.cos(X1m.flatten()))**(2)
+nu = h*np.ones(Nx) 
 
+Xi0init = mm.generateinitcostcone(X0,X,a,b,eps)
 Xi0 = mm.generatecostcone(X,X,a,b,eps)
-Xi1 = mm.generatecouplingcone(X,mm.fcone,a,b,eps)
+Xi1 = mm.generatecouplingcone(X,X0,mm.fcone,a,b,eps)
 
 # Alternate coupling assignment via permuation (odd N)
 #sigma = np.concatenate((np.arange(0,N,2),np.arange(N-2,0,-2)),axis=0)
 #Xi1 = mm.generatecost(X,X[sigma],eps*0.1)
 
 # Lagrange multiplier matrix 
-LUMAT = np.zeros([K,N*Nr])
+PMAT = np.zeros([K,Nx])
 
 tol = 10**-6
 err = 1.0
 
 ii = 0
 errv =[]
-G = [Xi0,Xi1]
+G = [Xi0init,Xi0,Xi1]
 #while err>tol:
-for ii in range(3000):
-    LUMAT, err = mm.fixedpoint(LUMAT,G,nu)
+for ii in range(100):
+    PMAT, err = mm.fixedpointcone(PMAT,G,X1,nu)
     errv.append(err)
     print err
 
@@ -59,7 +61,7 @@ for ii in range(3000):
 #k_map = int(K/2)
 k_map = int(K/2)
 
-Tmap = mm.computetransport(np.exp(LUMAT),k_map,[Xi0,Xi1])
+Tmap = mm.computetransportcone(PMAT,K-1,X1,[Xi0init,Xi0,Xi1])
 plt.imshow(-30*Tmap,origin='lower',cmap = 'gray')
 
 plt.show()
--- multimarg_fluids.py
+++ multimarg_fluids.py
@@ -1,8 +1,9 @@
 """
 Module containing classes and functions for computation of a multimarginal problem for fluids 
 """
-import numpy as np
 
+import numpy as np
+import scipy.optimize as so
 
 def S(x):
     #return 1.0 - x
@@ -10,60 +11,6 @@ def S(x):
         return 2.0*x 
     else:
         return 2.0 -2.0*x
-
-def fcone(x):
-    return [x, 1.0+0.0*x]
-
-
-
-def costcone(x0,x1,y0,y1,a,b):
-    if (a/(2.0*b)*abs(x0-y0))< np.pi:
-        return 4*b**2*(y1+x1-2*np.sqrt(y1*x1)*np.cos(a/(2.0*b)*abs(x0-y0)))
-    else:
-        return 4*b**2*(y1+x1+2*np.sqrt(y1*x1))
-
-
-
-def generatecostcone(X,Y,a,b,sigma):
-    """ Generate cost using cone metric using inverse tangent coordinates for radial dimension
-      
-        :param X: X[0] meshgrid base space coordinates
-                  X[1] \in ]0,pi/2[ meshgrid  inverse tangent of cone coordinates 
-        :param Y: Y[0] meshgrid base space coordinates
-                  Y[1] \in ]0,pi/2[ meshgrid inverse tangent of cone coordinates
-
-        :returns cost: cost matrix
-    """
-    
-    Nx = X[0].size
-    Ny = Y[0].size
-    vcostcone = np.vectorize(costcone)
-
-    cost = np.exp(-vcostcone(np.tensordot(X[0].flatten(),np.ones(Ny),axes =0),
-                            np.tensordot(np.tan(X[1].flatten()),np.ones(Ny),axes =0),
-                            np.tensordot(np.ones(Nx),Y[0].flatten(),axes =0),
-                            np.tensordot(np.ones(Nx),np.tan(Y[1].flatten()),axes =0),a,b)/sigma)
-   
-    return cost
-
-
-
-def generatecouplingcone(X,fundet,a,b,sigma):
-    """Generates L2 distance cost function penalising coupling 
-        
-       :param X: X[0] meshgrid base space coordinates
-                 X[1] \in [0,pi/2[ meshgrid inverse tangent of cone coordinates 
-       :param fundet: fundet[0] function defined on scalar x definining the coupling
-                      fundet[1] Jacobian determinant
-       :param eps: regularisation parameter (Sinkhorn)
-
-       :returns cost: cost matrix
-    """   
-    
-   
-    fundetx = fundet(X[0])
-    cost = generatecostcone(X,[fundetx[0],np.arctan(fundetx[1]*np.tan(X[1]))],a,b,sigma)
-    return cost   
 
 
 def generatecost(x,y,sigma):
@@ -174,5 +121,250 @@ def computetransport(UMAT,k_map,G):
         for ss in range(k_map+1,K):
              temp_kernel = (temp_kernel*UMAT[ss,:]).dot(G[setcurrentkernel(ss,K)])
         return (temp_kernel*UMAT[0,:])*((temp_kernel2*UMAT[k_map,:]).T)
+
+"""
+Functions for CH solutions on cone : 
+    Implementation using m =sqrt(r) change of variable
+    - metric on cone  m g + dm^2/m 
+    - distance on cone m1 + m2 - 2*sqrt(m1 m2) cos( d(x1,x2) wedge pi) 
+    - map (x,m) = (phi,Jac(phi))
+"""
+
+
+def fcone(x):
+    return [(np.exp(x)-1.0)/(np.exp(1)-1),np.exp(x)/(np.exp(1)-1)]
+
+
+def costcone(x0,x1,y0,y1,a,b):
+    """ Computes cost on cone 
+         
+        :param x0: base space component of first point
+        :param x1: radial (m) component of first point
+        :param y0: base space component of second point
+        :param y1: radial (m) component of second point
+        :param a,b: parameters for metric, a=1, b=1/2 corresponds to cone metric in standard coordinates
+    """
+
+
+    if (a/(2.0*b)*abs(x0-y0))< np.pi:
+        return 4*b**2*(y1+x1-2*np.sqrt(y1*x1)*np.cos(a/(2.0*b)*(x0-y0)))
+    else:
+        return 4*b**2*(y1+x1+2*np.sqrt(y1*x1))
+
+
+def generatecostcone(X,Y,a,b,sigma):
+    """ Generate cost using cone metric
+      
+        :param X: X[0] flattened meshgrid base space coordinates
+                  X[1] \in log ]0,M] flattened meshgrid radial cordinates 
+        :param Y: Y[0] flattened meshgrid base space coordinates
+                  Y[1] \in log ]0,M] flattened meshgrid radial coordinates
+
+        :returns cost: cost matrix
+    """
+    
+    Nx = X[0].size
+    Ny = Y[0].size
+    vcostcone = np.vectorize(costcone)
+
+    cost = np.exp(-vcostcone(np.tensordot(X[0].flatten(),np.ones(Ny),axes =0),
+                            np.tensordot(X[1].flatten(),np.ones(Ny),axes =0),
+                            np.tensordot(np.ones(Nx),Y[0].flatten(),axes =0),
+                            np.tensordot(np.ones(Nx),Y[1].flatten(),axes =0),a,b)/sigma)
+   
+    return cost
+
+
+
+def generateinitcostcone(x,Y,a,b,sigma):
+    """ Generate cost relative to initial time (when radial coordinate is fixed to 1) using cone metric
+      
+        :param x: array of base space coordinates
+        :param Y: Y[0] flattened meshgrid base space coordinates
+                  Y[1] \in log ]0,M] flattened meshgrid radial coordinates
+
+        :returns cost: cost matrix
+    """
+    
+    Nx = x.size
+    r = np.ones(Nx)
+    Ny = Y[0].size
+    vcostcone = np.vectorize(costcone)
+
+    cost = np.exp(-vcostcone(np.tensordot(x,np.ones(Ny),axes =0),
+                            np.tensordot(r,np.ones(Ny),axes =0),
+                            np.tensordot(np.ones(Nx),Y[0].flatten(),axes =0),
+                            np.tensordot(np.ones(Nx),Y[1].flatten(),axes =0),a,b)/sigma)
+   
+    return cost
+
+
+
+def generatecouplingcone(X,x,fundet,a,b,sigma):
+    """Generates L2 distance cost function penalising coupling 
+        
+       :param X: X[0] flattened meshgrid base space coordinates
+                 X[1] \in ]0,M] flattened meshgrid cone coordinates
+       :param x: array of base space coordinates          
+       :param fundet: fundet[0] function defined on scalar x definining the coupling
+                      fundet[1] Jacobian determinant
+       :param eps: regularisation parameter (Sinkhorn)
+
+       :returns cost: cost matrix (rectangular Nx*Nr times Nx)
+    """   
+    
+   
+    fundetx = fundet(x)
+    cost = generatecostcone(X,[fundetx[0],fundetx[1]],a,b,sigma)
+    return cost   
+
+def setcurrentkernelcone(k,K):
+    """Returns flag which tells if we are at last time step
+       
+       :param k: current time step (from 0 to K-1)
+       :param K: total number of time steps
+
+       :returns: 0 for k=0, 1 for 1<k<K-1, 2 for k=K-1
+    """
+    return 0 + (k>0.1) + (k==K-1) 
+
+def liftmultipliercone(p,y):
+    """ Maps Lagrange multiplier on base space to one on cone 
+ 
+    :param p: lagrange multiplier (len= Nx)  array
+    :param y: coordinates in radial direction
+ 
+    :returns: lagrange multiplier (len= Nx*Nr)
+    """
+    Nr = len(y)
+    return (np.exp(((np.tile(p,(Nr,1)).T)*y)).T).flatten() 
+
+
+def computepseudomarginalcone(k,K,y,G,PMAT):
+    """Computes kth pseudo marginal (needs to be multiplied by kth Lagrange multiplier to produce the kth marginal).
+       Used for update of kth Lagrange multiplier
+  
+       :param k: current time step (from 0 to K-1)
+       :param K: total number of time steps
+       :param y: array of coordinates in radial direction
+       :param G: list G = [Xi0init,Xi0,Xi1]
+       :param Xi0init: cost associated with first and second time steps
+       :param Xi0: cost associated to successive time steps
+       :param Xi1: cost associated to coupling
+       :param PMAT: array containing Lagrange multipliers (rows) in log scale to enforce marginals 
+    """ 
+    sequence = np.roll(range(K),-k-1)
+    temp_kernel = G[setcurrentkernelcone(k,K)]
+
+    for ii in sequence[:-1]:
+        if ii == 0:
+            U = np.exp(PMAT[ii,:])
+        else:
+            U = liftmultipliercone(PMAT[ii,:],y)
+        temp_kernel = (temp_kernel*U).dot(G[setcurrentkernelcone(ii,K)])
+
+    return np.diag(temp_kernel)
+
+
+def computemultipliercone(p_old,pseudomarg,y,nu):
+    """Computes updated Lagrange multiplier (after the first time step) by solving nonlinear equation
+       
+       :param p_old: old Lagrange multiplier
+       :param pseudomarg: pseudo marginal array
+       :param y: radial coordinate vector
+       :param nu: marginal on base space 
+    """
+    
+    def objective(p,i,pseudomargmat,y,nu):
+        """ Computes marginal residual """
+        
+        #Nx = len(nu)
+        Ny = len(y)
+        #newDensity = np.zeros(Nx)
+        newDensity = 0.0
+        for j in range(Ny):
+            newDensity +=  pseudomargmat[j,i]*np.exp(p*y[j])*y[j]
+        
+        return np.log(newDensity) -np.log(nu[i])
+    
+    Nx = len(nu)
+    Ny = len(y)
+    # IS THIS CORRECT?
+    pseudomargmat = pseudomarg.reshape(Ny,Nx)
+    p_new=p_old
+    for i in range(Nx):
+        dico = so.root(objective,1.0,args=(i,pseudomargmat,y,nu),method="broyden1")
+        p_new[i] = dico["x"]
+    return p_new
+
+
+
+
+def fixedpointcone(PMAT,G,y,nu):
+   """Fixed point map on Lagrange multipliers for multimarginal problem
+       
+      :param PMAT: array containing logarithm of Lagrange multipliers (rows) to enforce marginals
+      :param nu: marginal to be enforced at each time (on base space)
+      :param G: list G = [Xi0init,Xi0,Xi1]
+      :param Xi0init: cost associated to first and second time steps
+      :param Xi0: cost associated to successive time steps
+      :param Xi1: cost associated to coupling
+      :param nu: marginal to be enforced at each time
+   
+      :returns LUMAT: updated LUMAT
+      :returns err: marginal deviation from previous iteration at time K/2  
+   """
+   Nx = PMAT.shape[1] #Number of cells
+   K =  PMAT.shape[0] #Number of time steps
+ 
+   # Change of vaiables
+   
+   temp = computepseudomarginalcone(0,K,y,G,PMAT)   
+   PMAT[0,:] = np.log(nu/temp)
+   for imod in range(1,K):
+       # Each iteration updates the row (time level) imod in UMAT
+       temp = computepseudomarginalcone(imod,K,y,G,PMAT)
+       Pnew =  computemultipliercone(PMAT[imod,:],temp,y,nu)
+       if imod == int(K/2):
+            newDensity = 0.0
+            U =  liftmultipliercone(PMAT[imod,:],y)
+            marg = np.sum((temp*U).reshape(len(y),Nx).T*y,axis=1)
+            err = np.sum(np.abs(marg-nu))
+            #err = np.sum(np.abs(PMAT[imod,:]))
+       PMAT[imod,:] = Pnew
+
+
+   return PMAT, err
+
+
+def computetransportcone(PMAT,k_map,y,G):
+    """Compute plan time step 0  -> time step k_map
+ 
+       :param PMAT: array containing Lagrange multipliers (rows) to enforce marginals
+       :param k_map: time step (needs to be 0<k_map<K-1) at which computing the coupling
+       :param G: list G = [Xiinit, Xi0,Xi1]
+       :param Xi0: cost associated to successive time steps
+       :param Xi1: cost associated to coupling
+
+       :returns T: transport plan   
+    """
+    Nx = PMAT.shape[1]
+    K = PMAT.shape[0]
+    if k_map == 0:
+        return np.eye(N)/N
+    else:
+        temp_kernel2 = G[0]
+        for ss in range(1,k_map):
+             U = liftmultipliercone(PMAT[ss,:],y)
+             temp_kernel2 = (temp_kernel2*U).dot(G[1])
+        temp_kernel = G[setcurrentkernelcone(k_map,K)]
+        for ss in range(k_map+1,K):
+              U = liftmultipliercone(PMAT[ss,:],y)
+              temp_kernel = (temp_kernel*U).dot(G[setcurrentkernelcone(ss,K)])
+            
+    U = liftmultipliercone(PMAT[k_map,:],y)
+    T_tot = (temp_kernel*np.exp(PMAT[0,:]))*((temp_kernel2*U).T)
+    return np.sum(T_tot.reshape(len(y),Nx,Nx),axis=0)
+
 
 
