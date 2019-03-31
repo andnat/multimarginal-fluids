5c2d02875cb73657a1ae906e07b86d78f71872ab
Andrea Natale <anatale@pl23-pro.paris.inria.fr>
2019-03-31 11:47:31 +0200

    Simplified version

--- chconesolver.py
+++ chconesolver.py
@@ -12,20 +12,16 @@ import time as time
 import matplotlib.pyplot as plt
 
 
-load_flag = True 
-inputpath = 'Figs/Testpeakoncont'
+#Periodic domain
+periodic_flag = True
 
-if load_flag:
-    sys.path.append(inputpath)
-    from logfile import *
-else:    
-    Nx = 40 #28#35 
-    Nr = 41
-    K = 16
-    eps = 0.005
-    rmin =0.05
-    rmax= 2.05
-
+# Discretization parameters 
+Nx = 21 # Space discretization 
+Nr = 21 # Radial discretization
+K = 36 # Time discretization
+eps = 0.001 # Entropic regularization
+rmin =0.7 # Lower radial bound
+rmax= 1.3 # Upper radial bound
 
 # Parameters for cone metric
 a = 1.0
@@ -33,48 +29,37 @@ b= 0.5
 L = 1.0
 h = L/Nx
 
+# Discretization
 X0 = np.linspace(0.0,L,Nx)
-#X1 = np.asarray([1.0]) 
-
 X1 =np.linspace(rmin,rmax,Nr)
+
+#X1 = np.asarray([1.0]) 
 X0m,X1m = np.meshgrid(X0,X1)
 X =[X0m,X1m]
 
 # Marginal using x1 = arctan(r) change of variable
 nu = h*np.ones(Nx) 
 
-
+# Coputation in logarithmic scale
 log_flag = False 
 
-Xi0init = mm.generateinitcostcone(X0,X,a,b,eps, log_flag=log_flag)
-Xi0 = mm.generatecostcone(X,X,a,b,eps, log_flag=log_flag)
-Xi1 = mm.generatecouplingcone(X,X0,mm.peakon, mm.peakondet,a,b,eps)
-#Xi1 = mm.generatecouplingcone(X,X0,mm.S,1.0,a,b,eps,log_flag=log_flag)
+# Generates cost matrices
+Xi0init = mm.generateinitcostcone(X0,X,a,b,eps, log_flag=log_flag,periodic_flag=periodic_flag)
+Xi0 = mm.generatecostcone(X,X,a,b,eps, log_flag=log_flag,periodic_flag=periodic_flag)
 
-
-# Alternate coupling assignment via permuation (odd N)
-#sigma = np.concatenate((np.arange(0,N,2),np.arange(N-2,0,-2)),axis=0)
-#Xi1 = mm.generatecost(X,X[sigma],eps*0.1)
+# Generates coupling matrix
+Xi1 = mm.generatecouplingcone(X,X0,mm.peakon, mm.peakondet,a,b,eps,periodic_flag=periodic_flag)
 
 # Lagrange multiplier matrix 
-if load_flag:
-    PMAT = np.load('%s/%s' %(inputpath,"PMAT.npy"))
-    errv = np.load('%s/%s' %(inputpath,"errv.npy"))
-    errv = errv.tolist()
-    
-else: 
-    PMAT = np.zeros([K,Nx])
-    errv = []
-    
-
+PMAT = np.zeros([K,Nx])
+errv = []
 
 G = [Xi0init,Xi0,Xi1]
 
-# STANDARD ITERATION METHOD
-for ii in range(5000):
+# SINKHORN ITERATIONS
+for ii in range(1000):
     t = time.time()
     PMAT, err = mm.fixedpointconeroll(PMAT,G,X1,nu,verbose= False)
-    #PMAT, err, S = mm.fixedpointconerollback(PMAT,S,G,X1,nu)
     errv.append(err)
     elaps= time.time()-t
     print("ITERATION %d" % ii)
@@ -82,34 +67,7 @@ for ii in range(5000):
     print("Marginal error: %f" % err)
 
 
-# ANDERSON ITERATION METHOD
-#params = [G,X1,nu]
-#iterations_simple = 50
-#iterations_anderson = 350 #43#400#60 
-#number_of_steps = 1 
-#memory_number = 10 #40 
-#
-#PMAT, errv1 = mm.OptimizationAndersonMixed(mm.fixedpointconeroll,PMAT, iterations_simple,iterations_anderson,number_of_steps,memory_number,params)
-#errv += errv1 
+# SAVE FIGURES
+mm.savefigscone(errv,PMAT, X1, eps ,G, savepath , ext='eps')
+mm.savedatacone(errv,PMAT,X1,eps, savepath)
 
-path = "Figs/Testpeakoncont1"
-mm.savefigscone(errv,PMAT, X1, eps ,G, path , ext='eps')
-mm.savedatacone(errv,PMAT,X1,eps, path)
-
-#fig = plt.semilogy(errv)
-#plt.savefig(('Figs/Convergence.eps' %k_map),format = "eps")
-# 
-#
-#for k_map in range(K):
-#    Tmap = mm.computetransportcone(PMAT,k_map,X1,G,conedensity_flag = False, log_flag=log_flag)
-#    fig = plt.imshow(-30*Tmap,origin='lower',cmap = 'gray')
-#    fig.axes.get_xaxis().set_visible(False)
-#    fig.axes.get_yaxis().set_visible(False)
-#    plt.savefig(('Figs/transport0_%d.eps' %k_map),format = "eps")
-#    Tconemap = mm.computetransportcone(PMAT,k_map,X1,G,conedensity_flag=True, log_flag=log_flag)
-#    fig = plt.imshow(-30*Tconemap,origin='lower',cmap = 'gray')
-#    fig.axes.get_xaxis().set_visible(False)
-#    fig.axes.get_yaxis().set_visible(False)
-#    plt.savefig(('Figs/radialmarg0_%d.eps' %k_map),format = "eps")
-#
-#
